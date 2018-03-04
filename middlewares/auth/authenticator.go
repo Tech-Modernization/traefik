03fb8231343e73e4f2f2a04eb6e33c72d1363ccb
Taliesin Sisson <taliesins@yahoo.com>
2018-03-04 21:12:50 +0000

    Add Jwt Validator

--- middlewares/auth/authenticator.go
+++ middlewares/auth/authenticator.go
@@ -56,6 +56,10 @@ func NewAuthenticator(authConfig *types.Auth, tracingMiddleware *tracing.Tracing
 		tracingAuthenticator.handler = createAuthForwardHandler(authConfig)
 		tracingAuthenticator.name = "Auth Forward"
 		tracingAuthenticator.clientSpanKind = true
+	} else if authConfig.Jwt != nil && ((authConfig.Jwt.HS256 != nil && authConfig.Jwt.HS256.ClientSecret != "") || (authConfig.Jwt.RS256 != nil && authConfig.Jwt.RS256.JwksTargetIssuer != "")) {
+		tracingAuthenticator.handler = createAuthJwtHandler(authConfig)
+		tracingAuthenticator.name = "Auth Jwt"
+		tracingAuthenticator.clientSpanKind = false
 	}
 	if tracingMiddleware != nil {
 		authenticator.handler = tracingMiddleware.NewNegroniHandlerWrapper(tracingAuthenticator.name, tracingAuthenticator.handler, tracingAuthenticator.clientSpanKind)
@@ -65,6 +69,9 @@ func NewAuthenticator(authConfig *types.Auth, tracingMiddleware *tracing.Tracing
 	return &authenticator, nil
 }
 
+func createAuthJwtHandler(authConfig *types.Auth) negroni.HandlerFunc {
+	return Jwt(authConfig.Jwt)
+}
 func createAuthForwardHandler(authConfig *types.Auth) negroni.HandlerFunc {
 	return negroni.HandlerFunc(func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 		Forward(authConfig.Forward, w, r, next)
